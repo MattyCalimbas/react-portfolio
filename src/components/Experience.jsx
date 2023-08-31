b3bc478ab014b4b251be8ec599faaee257217a69
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-31 11:15:13 -0700

    adjusted lighting

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, Sky, Environment } from "@react-three/drei";
+import { OrbitControls, Sky, Environment, ContactShadows } from "@react-three/drei";
 import Avatar from './Avatar';
 import { useControls } from "leva";
 
@@ -10,18 +10,27 @@ export default function Experience() {
       options: ["Typing", "Falling", "Standing"]
     }
   })
+
+  let box;
+  if (animation === "Typing") {
+    box = (
+      <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+        <boxGeometry />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    )
+  }
+
   return (
     <>
       <OrbitControls />
       <Sky />
       <Environment preset='sunset' />
       <group position-y={-1}>
+      <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
         <Avatar animation={animation} />
-        <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
-          <boxGeometry />
-          <meshStandardMaterial color="white" />
-        </mesh>
-        <mesh receiveShadow scale={5} rotation-x={-Math.PI * 0.5}>
+        {box}
+        <mesh receiveShadow scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
           <planeGeometry />
           <meshStandardMaterial color='white' />
         </mesh>
