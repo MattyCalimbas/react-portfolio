e7b611a3212865793a8e7c8ac583b3e2d17f772b
Matt Calimbas <matthewcalimbas@gmail.com>
2023-09-06 13:02:19 -0700

    added tailwind css

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls, Sky, Environment, ContactShadows } from "@react-three/drei";
 import Avatar from './Avatar';
 import { useControls } from "leva";
-import Office from './Office'
+// import Office from './Office'
 
 export default function Experience() {
 
@@ -36,7 +36,7 @@ export default function Experience() {
           <meshStandardMaterial color='white' />
         </mesh>
       </group>
-      <Office />
+      {/* <Office /> */}
     </>
   );
 };
