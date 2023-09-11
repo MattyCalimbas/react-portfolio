dc456b0de25239fead8c7992917ad9a433a4d60c
Matt Calimbas <matthewcalimbas@gmail.com>
2023-09-11 11:03:16 -0700

    added hamburger menu component

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,25 @@ import Experience from "./components/Experience";
 import { ScrollControls, Scroll } from "@react-three/drei";
 import Interface from "./components/Interface";
 import ScrollManager from "./components/ScrollManager"
+import Menu from './components/Menu';
 function App() {
   const [section, setSection] = useState(0);
-  
-  return (
-    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <ScrollControls pages={4} damping={0.1}>
-        <ScrollManager section={section} onSectionChange={setSection} />
-        <Experience />
-        <Scroll html>
-          <Interface />
-        </Scroll>
-      </ScrollControls>
-    </Canvas>
+  const [menuOpened, setMenuOpened] = useState(false);
 
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={4} damping={0.1}>
+          <ScrollManager section={section} onSectionChange={setSection} />
+          <Experience />
+          <Scroll html>
+            <Interface />
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+      <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
+    </>
   );
 }
 
