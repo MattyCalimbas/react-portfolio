5d0f47f19cea146f1d0bd526ed6eb2deaf5d47e1
Matt Calimbas <matthewcalimbas@gmail.com>
2023-09-11 12:30:06 -0700

    added MotionConfig in app for office

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,39 @@ import { ScrollControls, Scroll } from "@react-three/drei";
 import Interface from "./components/Interface";
 import ScrollManager from "./components/ScrollManager"
 import Menu from './components/Menu';
+import { MotionConfig } from 'framer-motion';
 function App() {
   const [section, setSection] = useState(0);
   const [menuOpened, setMenuOpened] = useState(false);
 
 
   // useEffect rather then onClick so menu closes when scrolling
-  useEffect(()=>{
+  useEffect(() => {
     setMenuOpened(false);
   }, [section])
 
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
-        <color attach="background" args={["#e6e7ff"]} />
-        <ScrollControls pages={4} damping={0.1}>
-          <ScrollManager section={section} onSectionChange={setSection} />
-          <Experience section={section} />
-          <Scroll html>
-            <Interface />
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-      <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
+      <MotionConfig transition={{
+        type: "spring",
+        mass: 5,
+        stiffness: 500,
+        damping: 50,
+        restDelta: 0.0001
+      }}>
+        <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+          <color attach="background" args={["#e6e7ff"]} />
+          <ScrollControls pages={4} damping={0.1}>
+            <ScrollManager section={section} onSectionChange={setSection} />
+            <Experience section={section} />
+            <Scroll html>
+              <Interface />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
+      </MotionConfig>
+
     </>
   );
 }
